956fe52deed5c0c01d457dd01d130ba7e6a7fdaa
AnthoniIP <anthoni.ipiranga@gmail.com>
2021-02-23 19:19:48 -0300

    adicionado exercicio 4

---
+++ lista2/4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+ 
+int main(void){
+ 
+float dividendo = 1, resultado = 0;
+ 
+for (float divisor = 1; dividendo <= 50; dividendo++, divisor + 2){
+   resultado += dividendo/divisor;
+}
+ 
+printf("%f", resultado);
+ 
+return 0;
+}
